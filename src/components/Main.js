a0d3a82c4a3c73c59552d79d8f56ef71aee01f2f
Koding Kidd <kiddkoding@gmail.com>
2024-09-06 21:33:45 -0700

    initial commit

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Main = () => {
+  return (
+    <section className="main">
+      <h1>I'm a former businessman seeking to become a Software Developer.
+      </h1>
+    </section>
+  );
+};
+
+export default Main;
